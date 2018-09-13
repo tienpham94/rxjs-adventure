abe92d1cb565353e87ae63ebb810afc908dd2c3a
Tien Pham <r3dsky02@yahoo.com.vn>
2018-09-13 22:35:27 +0300

    Working observable

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,11 @@ import { Observable } from "rxjs";
 
 const observable = Observable.create((observable: any) => {
   observable.next("Hello world");
+  observable.next("Buddy");
 });
+
+observable.subscribe((x: any) => console.log(x));
+
+// const logItem = (val: any) => {
+//   const node = document.createElement()
+// }
